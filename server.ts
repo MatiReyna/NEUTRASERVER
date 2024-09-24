16718f94fac53b8357e9b00ae8517b292fcef0a1
Matias Reyna <matiireyna@hotmail.com>
2024-09-24 18:26:45 -0300

    Sincronizacion de base de datos: done

--- server.ts
+++ server.ts
@@ -1,12 +1,19 @@
 import server from './src/app';
 import dotenv from 'dotenv';
+import sequelize from './src/db';
+
+// <-- Importamos los modelos -->
+import './src/models/product.models';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
-function startServer() {
+async function startServer() {
     try {
+        await sequelize.sync({ force: false });
+        console.log('Database synced successfully');
+
         server.listen(PORT, () => {
             console.log(`Server raised in port: ${PORT}`)
         });
---
+++ src/models/product.models.ts
@@ -0,0 +1,30 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../db';
+
+class Product extends Model {}
+
+Product.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    price: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    },
+    stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+}, { sequelize, modelName: 'Product' });
+
+export default Product;
